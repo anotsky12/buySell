b410fa8036f15d501d09b8195c72f57d97e05e4a
ANotSky <anotsky12@gmail.com>
2023-09-23 14:55:10 +0300

    buysell

---
+++ src/main/java/com/anotsky/buysell/BuySellApplication.java
@@ -0,0 +1,14 @@
+package com.anotsky.buysell;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuySellApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/anotsky/buysell/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.anotsky.buysell.repositories;
+
+import com.anotsky.buysell.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
